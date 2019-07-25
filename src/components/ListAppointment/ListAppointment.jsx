03765fc13a0f191f7149ef307d72f23db1195076
Damyan Peykov <dppeykov@gmail.com>
2019-07-25 15:17:21 +0200

    Ending video 7 section 2

--- src/components/ListAppointment/ListAppointment.jsx
+++ src/components/ListAppointment/ListAppointment.jsx
@@ -1,9 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ListAppointment extends Component {
-  render() {
-    return <div>List appointment</div>;
-  }
-}
+const ListAppointment = ({ appointments }) => {
+  return (
+    <div className="appointment-list item-list mb-3">
+      {appointments.map(appointment => (
+        <div className="pet-item col media py-3" key={appointments.aptId}>
+          <div className="mr-3">
+            <button className="pet-delete btn btn-sm btn-danger">X</button>
+          </div>
+
+          <div className="pet-info media-body">
+            <div className="pet-head d-flex">
+              <span className="pet-name"> {appointment.petName} </span>
+              <span className="apt-date ml-auto"> {appointment.aptDate} </span>
+            </div>
+
+            <div className="owner-name">
+              <span className="label-item">Owner: </span>
+              <span>{appointment.ownerName}</span>
+            </div>
+
+            <div className="apt-notes"> {appointment.aptNotes} </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ListAppointment;
