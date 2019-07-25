ccf533664f335dafc4f0ac919d8716abf014741a
Damyan Peykov <dppeykov@gmail.com>
2019-07-25 12:48:31 +0200

    Finishing Section 1

--- src/components/App/App-component.jsx
+++ src/components/App/App-component.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>rendering</h1>
-    </div>
+    <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
+              <div>Add Appointment</div>
+              <div>Search Appointment</div>
+              <div>List Appointment</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
