e22b1afeaa27f91cb0bf382749c3cc83212d4255
Damyan Peykov <dppeykov@gmail.com>
2019-07-25 12:41:31 +0200

    Adding some changes
    Final setup of the project - can start working on it

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "./styles.css";
+import "./css/styles.css";
 
 import "jquery/dist/jquery.js";
 import "popper.js/dist/umd/popper.js";
 import "bootstrap/dist/js/bootstrap.js";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>rendering</h1>
-    </div>
-  );
-}
+import App from "./components/App/App-component";
 
 ReactDOM.render(<App />, document.getElementById("root"));
