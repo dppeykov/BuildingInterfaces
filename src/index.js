952bc3e19e1b74c47198d7204585d1b0045ee17f
Damyan Peykov <dppeykov@gmail.com>
2019-07-25 12:16:39 +0200

    Fixing the bootstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
+
+import "jquery/dist/jquery.js";
+import "popper.js/dist/umd/popper.js";
+import "bootstrap/dist/js/bootstrap.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial load</h1>
+      <h1>rendering</h1>
     </div>
   );
 }
